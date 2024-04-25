fca919793a4f0f4241e5f128e72b33b1382dd1fc
KMWH <kmwh030@gmail.com>
2024-04-25 23:36:48 +0900

    week6

---
+++ week6/stack_quiz.c
@@ -0,0 +1,99 @@
+// 202112304 김원호
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+typedef int element;
+typedef struct {
+	element data[MAX];
+	int top;
+}StackType;
+
+void init_stack(StackType* s) {
+	s->top = -1;
+}
+
+int is_full(StackType* s) {
+	return (s->top == (MAX - 1));
+}
+
+int is_empty(StackType* s) {
+	return (s->top == -1);
+}
+
+void push(StackType* s, element item) {
+	if (is_full(s)) {
+		fprintf(stderr, "스택 overflow\n");
+		return;
+	}
+	else s->data[++(s->top)] = item;
+}
+
+element pop(StackType* s) {
+	if (is_empty(s)) {
+		fprintf(stderr, "스택 underflow\n");
+		exit(1);
+	}
+	else {
+		return s->data[(s->top)--];
+	}
+}
+
+int main()
+{
+	int infinity = 87654321;
+	int problem_num = 0;
+	
+	scanf("%d", &problem_num);
+
+	for (int problem = 0; problem < problem_num; problem++) {
+		int query_num = 0;
+		StackType min_stack;
+		StackType max_stack;
+
+		scanf("%d", &query_num);
+
+		init_stack(&min_stack);
+		init_stack(&max_stack);
+		push(&min_stack, infinity);
+		push(&max_stack, -infinity);
+
+		for (int query = 0; query < query_num; query++) {
+			int input1, input2, new_block = 0;
+
+			scanf("%d", &input1);
+
+			switch (input1) {
+			case 1:
+				scanf("%d", &input2);
+				new_block = input2;
+
+				if (min_stack.data[min_stack.top] > new_block) {
+					push(&min_stack, new_block);
+				}
+				else {
+					push(&min_stack, min_stack.data[min_stack.top]);
+				}
+				if (max_stack.data[max_stack.top] < new_block) {
+					push(&max_stack, new_block);
+				}
+				else {
+					push(&max_stack, max_stack.data[max_stack.top]);
+				}
+				printf("%d %d\n", min_stack.data[min_stack.top], max_stack.data[max_stack.top]);
+				break;
+			case 2:
+				if (min_stack.top != infinity) {
+					pop(&min_stack);
+				}
+				if (max_stack.top != -infinity) {
+					pop(&max_stack);
+				}
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
